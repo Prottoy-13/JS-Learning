6fe5f6123f818e40b3d9df763855e638bc9a0019
Prottoy Mankin <prottoy032020@gmail.com>
2024-12-04 14:35:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" //Treat all the JS code as newer version.
+
+//alert(3 + 3) //we are using nodejs, not browser.
+
+// Code readability should be high
+
+//null => standalone value
+
+// symbol => unique
+
+let age = 21
+console.log(typeof age);
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
